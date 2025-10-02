367877763da3f4b112924d8de4b302d98ddfd423
Joaquín CV <96045961+JCerroV@users.noreply.github.com>
2025-10-02 13:03:08 +0200

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,7 +4,7 @@ export const projects = [
         title: "GameShop",
         description:"gameshop",
         tech: ["MySQL","Spring Boot","Java", "Angular","TypeScript", "Bootstrap"],
-        github: "https://github.com/tuusuario/portfolio",
+        github: "https://github.com/JCerroV/GameShop",
         demo: "https://tu-portfolio.vercel.app",
         image: "/images/portfolio.png",
     },
@@ -12,8 +12,8 @@ export const projects = [
         title: "Portfolio Web",
         description:"portfolio",
         tech: ["React","JavaScript", "Tailwind CSS", "Vite"],
-        github: "https://github.com/tuusuario/portfolio",
+        github: "https://github.com/JCerroV/MyPortfolio",
         demo: "https://tu-portfolio.vercel.app",
-        image: "/images/portfolio.png",  
+        image: "../assets/portfolio.png",  
     }
 ]
